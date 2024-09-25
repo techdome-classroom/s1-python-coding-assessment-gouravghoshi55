6550606e7d49b19d93eeae45cfe40cc2aad85a53
gouravghoshi55 <149483422+gouravghoshi55@users.noreply.github.com>
2024-09-25 12:58:51 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,16 @@
-def decode_message( s: str, p: str) -> bool:
-
-# write your code here
-  
-        return False
+def decode_message(s: str, p: str) -> bool:
+    dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+    dp[0][0] = True
+    
+    for j in range(1, len(p) + 1):
+        if p[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+    
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(p) + 1):
+            if p[j - 1] == s[i - 1] or p[j - 1] == '?':
+                dp[i][j] = dp[i - 1][j - 1]
+            elif p[j - 1] == '*':
+                dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+    
+    return dp[len(s)][len(p)]
