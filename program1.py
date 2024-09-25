e26c9c92a7b0c576b41f212a35d7a10f7522a5cc
gouravghoshi55 <149483422+gouravghoshi55@users.noreply.github.com>
2024-09-25 12:55:43 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,13 +1,11 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-     if not grid:
+        if not grid:
             return 0
         
-    rows, cols = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
         
-def dfs(r, c):
+        def dfs(r, c):
             if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W':
                 return
             grid[r][c] = 'W'
@@ -16,11 +14,12 @@ def dfs(r, c):
             dfs(r, c + 1)
             dfs(r, c - 1)
         
-total_isles = 0
+        total_isles = 0
         
-for i in range(rows):
+        for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 'L':
                     total_isles += 1
                     dfs(i, j)
-return total_isles
+        
+        return total_isles
